faf596c8fb08d3985a4c0c081ec51736e7d95d4f
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-10 13:33:24 +0530

    New Commit

--- React With Harry/React-App/src/App.jsx
+++ React With Harry/React-App/src/App.jsx
@@ -1,21 +1,24 @@
 // import './App.css'
-import {useState} from "react"
+import { useState } from "react"
 import NavBar from "./Component/NavBar.jsx"
 import Footer from "./Component/Footer.jsx"
 import Card from "./Component/Card.jsx"
 
 function App() {
-  
+
   return (
     <>
-      <NavBar/>
-
+      <NavBar />
+      <div className="cards">
+        
       <Card title = "This Is First Card" description="I am not Too much expencive"/>
       <Card title = "This Is Second Card" description="I am not Too much expencive"/>
       <Card title = "This Is Third Card" description="I am not Too much expencive"/>
 
-      <Footer/>
-    
+      </div>
+
+      <Footer />
+
 
     </>
   )
--- React With Harry/React-App/src/Component/Card.jsx
+++ React With Harry/React-App/src/Component/Card.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import "./Card.css"
 
 
-function Card({title,description}) {
+function Card({ title, description }) {
   return (
     <div className='card'>
-        <div>
+      <div>
+        <img src="https://cdn.sanity.io/images/tlr8oxjg/production/1ca7b34a8d5308a03ae186dfe72caabce0327fe2-1456x816.png?w=3840&q=100&fit=clip&auto=format" alt="" width={100} style={{alignItems:"center"}} />
         <h1 className='title'>{title}</h1>
         <p className='description'> {description}</p>
-      
-        </div>
+
+      </div>
     </div>
   )
 }
