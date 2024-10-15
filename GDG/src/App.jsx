0d79cdaca287f8a8f6cd66f560fb20e3cf03d2a2
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-15 22:59:43 +0530

    new commit

---
+++ GDG/src/App.jsx
@@ -0,0 +1,16 @@
+// import NavBar from "./Components/NavBar.jsx"
+import navbar from "./Components/NavBar.jax"
+
+
+function App() {
+
+
+  return (
+    <>
+   <NavBar/>
+     
+    </>
+  )
+}
+
+export default App
